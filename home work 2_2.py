a762eb40be1b8f8871b5d92a97ec7b92c3289c5f
lyubomyrr <46572442+lyubomyrr@users.noreply.github.com>
2019-01-21 13:36:01 +0200

    Update home work 2_2.py

--- home work 2_2.py
+++ home work 2_2.py
@@ -7,3 +7,6 @@ c = str(num)
 print("Number in reverse  ", c[::-1])
 s = str(num)
 print("Sorted out number  ", sorted(s))
+#Знайти добуток цифр цього числа.
+#Записати число в реверсному порядку.
+#Посортувати цифри, що входять в дане число
