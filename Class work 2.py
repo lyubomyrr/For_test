50b544d1d05885182d51740ff4bfd4f735a58225
Liubomyr Karp <lyubomur.karp@gmail.com>
2019-01-24 08:12:15 +0200

    Create Class work 2.py

---
+++ Class work 2.py
@@ -0,0 +1,49 @@
+#for i in range(0, 100, 2):
+ #   print(i)
+# for i in range(1, 100, 2):
+ #   print(i)
+  #  if i == 5:
+   #     break
+   # print(i)
+#print ("end")  
+# list_number=[2,4,5,8,9,10]
+#contain_odd=False
+#for item in list_number:
+ #   if not item % 2==0:
+  #      contain_odd=True
+   #     break
+#if contain_odd:
+ #   print ("there are odd numbers in the list")
+#else :
+ #   print("there are only even numbers in the list ")                  
+#list_number=[2,4,5,8,9,10]
+#contain_odd=False
+#for item in list_number:
+#    if not item % 2==0:
+ #       contain_odd=True
+  #      break
+#if contain_odd:
+ #   print ("there are odd numbers in the list")
+#else :
+ #   print("there are only even numbers in the list ")            
+#list_number= [32,52,12,52]
+#i=0
+#for a in list_number:
+ #   list_number[i] =float(a)
+  #  i = i + 1 
+   # print (list_number)
+#list_number=["433","banana","chary","tomato"]
+#for x in list_number:
+ #   for j in x:
+  #      print(j,end="#")
+   # print ()
+#list_number=[0, 1, 1, 2 ,3 , 5, 8, 13]
+#n=input("enter  ")
+#while n == list_number:
+ #   if n==3:
+  #      break
+#print ()
+
+
+
+
