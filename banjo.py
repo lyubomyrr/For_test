9dd8b1f10578b9d4916a24ebeec77e167731a8ae
Liubomyr Karp <lyubomur.karp@gmail.com>
2019-01-31 14:43:42 +0200

    Create banjo.py

---
+++ banjo.py
@@ -0,0 +1,6 @@
+def areYouPlayingBanjo(name):
+    if name[0] == "R" or name[0] == "r":
+        name = name + " plays banjo"
+    else:
+        name = name + " does not play banjo"
+    return name
