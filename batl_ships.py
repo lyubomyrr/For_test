1e7bd7b5b949e7cc7ef62cf4ab227d3fcb494e3a
lyubomyrr <46572442+lyubomyrr@users.noreply.github.com>
2019-02-21 19:37:16 +0200

    Update batl_ships.py

--- batl_ships.py
+++ batl_ships.py
@@ -1,14 +1,13 @@
-#python 2.7
 from random import randint
 
 board = []
 
 for x in range(0, 5):
-  board.append(["O"] * 5)
+  board.append(["O"] * 5)#add
 
 def print_board(board):
   for row in board:
-    print " ".join(row)
+    print (" ".join(row))
 
 print_board(board)
 
@@ -20,27 +19,27 @@ def random_col(board):
 
 ship_row = random_row(board)
 ship_col = random_col(board)
-print ship_row
-print ship_col
+print (ship_row)
+print (ship_col)
 
 
 for turn in range(4):
-  print "Turn", turn + 1
-  guess_row = int(raw_input("Guess Row: "))
-  guess_col = int(raw_input("Guess Col: "))
+  print ("Turn", turn + 1)
+  guess_row = int(input("Guess Row: "))
+  guess_col = int(input("Guess Col: "))
 
   if guess_row == ship_row and guess_col == ship_col:
-    print "Congratulations! You sank my battleship!"
+    print ("Congratulations! You sank my battleship!")
     break
   else:
     if guess_row not in range(5) or \
       guess_col not in range(5):
-      print "Oops, that's not even in the ocean."
+      print ("Oops, that's not even in the ocean.")
     elif board[guess_row][guess_col] == "X":
       print( "You guessed that one already." )
     else:
-      print "You missed my battleship!"
+      print ("You missed my battleship!")
       board[guess_row][guess_col] = "X"
     if (turn == 3):
-      print "Game Over"
+      print ("Game Over")
     print_board(board)
